b9a8bb718c278ff6eb3dd69a3fe61d7215b89d40
Bryan Kneis <b.kneis2011@gmail.com>
2017-10-03 16:50:24 +0100

    Create helper function for generating random number

---
+++ randomUtil.h
@@ -0,0 +1,20 @@
+//
+// Created by arthur on 03/10/17.
+//
+
+#ifndef GENETICALGORITHM_RANDOMUTIL_H
+#define GENETICALGORITHM_RANDOMUTIL_H
+
+//#include <random>
+//#include <chrono>
+#include <cstdlib>
+
+inline int getRandomNumber(int min, int max) {
+//    long seed1 = std::chrono::system_clock::now().time_since_epoch().count();
+//    std::default_random_engine gen(seed1);
+//    std::uniform_int_distribution<int> dist(min, max);
+//    return dist(gen);
+    return rand() % (max + 1);
+}
+
+#endif //GENETICALGORITHM_RANDOMUTIL_H
