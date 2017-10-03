d1749e3fc59a728438c0ca2d07e3007ade39fb3c
Bryan Kneis <b.kneis2011@gmail.com>
2017-10-03 16:50:41 +0100

    Use fluid syntax for GA flow

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 
+#include <cstdlib>
+#include <iostream>
 #include "Population.h"
+#include "config.h"
+#include "randomUtil.h"
 
 int main() {
-    auto population = new Population(30);
-    population->toString();
+    /* initialize random seed: */
+    srand (time(NULL));
+
+    auto * population = new Population();
+    population->generate(NUMBER_OF_CHROMOSONES);
+
+    for (int i = 0; i < NUMBER_OF_GENERATIONS; i++) {
+        std::cout << "Generation: " << i << "\n";
+        population = population
+                ->selectParents()
+                ->crossover()
+                ->mutate()
+                ->printStats();
+    }
     return 0;
 }
