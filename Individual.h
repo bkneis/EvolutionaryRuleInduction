20dc4e54b48d2abfc6f137d791cd80a7e3a912aa
Bryan Kneis <b.kneis2011@gmail.com>
2017-10-15 16:49:10 +0100

    Move declrations for template member functions to header

--- Individual.h
+++ Individual.h
@@ -2,27 +2,104 @@
 #define GENETICALGORITHM_INDIVIDUAL_H
 
 #include <stdint-gcc.h>
+#include "randomUtil.h"
+#include "config.h"
+#include <cmath>
+#include <iostream>
 
 enum fitnessStrategy { COUNTING_ONES, SQUARED };
 
+template <typename fitnessType = int>
 class Individual {
+
 public:
-    explicit Individual(int size, fitnessStrategy strategy);
-    void toString();
-    int getFitness();
-    int getSize();
-    void crossover(Individual *pIndividual);
-    void mutate();
-    int* getChromosomes();
-    int* setChromosomes(int* chromosome);
-    fitnessStrategy getFitnessStrategy();
+
+    Individual(int size, fitnessStrategy strategy) {
+        this->chromosome = new int[size];
+        this->fitnessStrat = strategy;
+        this->size = size;
+        for (int i = 0; i < size; i++) {
+            this->chromosome[i] = getRandomNumber(0, 1);
+        }
+    }
+
+    void toString() {
+        std::cout << "Chromosome ";
+        for (int i = 0; i < this->size; i++) {
+            std::cout << unsigned(this->chromosome[i]);
+        }
+        std::cout << " with fitness " << this->getFitness();
+    }
+
+    int getFitness() {
+        switch (this->fitnessStrat) {
+            case (COUNTING_ONES):
+                return this->getFitnessCountingOnes();
+            case (SQUARED):
+                return this->getFitnessSquared();
+            default:
+                std::cout << "Please choose an appropriate fitness strategy"; return 0;
+        }
+    }
+
+    void crossover(Individual<fitnessType>* partner) {
+        int point = getRandomNumber(0, NUMBER_OF_CHROMOSONES);
+        auto partnerChromosomes = partner->getChromosomes();
+        int tempBit;
+        for (int i = point; i < this->size; i++) {
+            tempBit = this->chromosome[i];
+            this->chromosome[i] = partnerChromosomes[i];
+            partnerChromosomes[i] = tempBit;
+        }
+        partner->setChromosomes(partnerChromosomes);
+    }
+
+    void mutate() {
+        int point = getRandomNumber(0, NUMBER_OF_CHROMOSONES);
+        this->chromosome[point] = 1 - this->chromosome[point];
+    }
+
+    int getSize() {
+        return this->size;
+    }
+
+    int* getChromosomes() {
+        return this->chromosome;
+    }
+
+    int* setChromosomes(int* chromosome) {
+        this->chromosome = chromosome;
+    }
+
+    fitnessStrategy getFitnessStrategy() {
+        return this->fitnessStrat;
+    }
 
 private:
-    int getFitnessCountingOnes();
-    int getFitnessSquared();
+
+    int getFitnessSquared() {
+        this->fitness = 0;
+        for (int i = 0; i < this->size; i++) {
+            if (this->chromosome[i] == 1) {
+                this->fitness += pow(2, i);
+            }
+        }
+        return this->fitness;
+    }
+
+    int getFitnessCountingOnes() {
+        this->fitness = 0;
+        for (int i = 0; i < this->size; i++) {
+            if (this->chromosome[i] == 1) {
+                this->fitness++;
+            }
+        }
+        return this->fitness;
+    }
+
     int* chromosome;
     int size;
-    int fitness;
+    fitnessType fitness;
     fitnessStrategy fitnessStrat;
 };
 
--- Population.h
+++ Population.h
@@ -2,27 +2,149 @@
 #define GENETICALGORITHM_POPULATION_H
 
 #include <vector>
+#include <cstring>
+#include <fstream>
+#include <iostream>
 #include "Individual.h"
 
+template <typename fitnessType = int>
 class Population {
 
 public:
-    std::vector<Individual*> individuals;
-    int getMaxFitness();
-    int getMeanFitness();
-    void toString();
-    void generate(int size);
-    void addIndividual(Individual* individual);
-    Population* calcStats();
-    Population* selectParents();
-    Population* crossover();
-    Population* mutate();
-    Population* printStats(std::string path);
-    Population* replaceWeakestIndividual(Individual* fittest);
-    Individual* getFitestIndividual();
-    Individual* cloneFittestIndividual();
+
+    std::vector<Individual<fitnessType>*> individuals;
+
+    void generate(int size) {
+        for (int i = 0; i < size; i++) {
+            this->individuals.push_back(new Individual<fitnessType>(NUMBER_OF_CHROMOSONES, COUNTING_ONES));
+        }
+    }
+
+    void toString() {
+        for (int i = 0; i < this->individuals.size(); i++) {
+            std::cout << "Individual at index " << i << " has ";
+            this->individuals.at(i)->toString();
+            std::cout << "\n";
+        }
+    }
+
+    Population* selectParents() {
+        auto tempPopulation = new Population<fitnessType>();
+
+        for (int i = 0; i < SIZE_OF_POPULATION; i++) {
+            auto parent1 = this->individuals.at(getRandomNumber<unsigned long> (0, SIZE_OF_POPULATION - 1));
+            auto parent2 = this->individuals.at(getRandomNumber<unsigned long> (0, SIZE_OF_POPULATION - 1));
+
+            if (parent1->getFitness() >= parent2->getFitness()) {
+                tempPopulation->addIndividual(parent1);
+            } else {
+                tempPopulation->addIndividual(parent2);
+            }
+        }
+        return tempPopulation;
+    }
+
+    Population* mutate() {
+        for (unsigned long i = 0; i < this->individuals.size() - 1; i++) {
+            if (getRandomNumber(1, 100) <= PROBABILITY_OF_MUTATION) {
+                auto individual = this->individuals.at(i);
+                individual->mutate();
+            }
+        }
+        return this;
+    }
+
+    Population* crossover() {
+        for (unsigned long i = 0; i < this->individuals.size() - 2; i += 2) {
+            if (getRandomNumber(1, 100) <= PROBABILITY_OF_CROSSOVER) {
+                auto parent1 = this->individuals.at(i);
+                auto parent2 = this->individuals.at(i + 1);
+                parent1->crossover(parent2);
+            }
+        }
+        return this;
+    }
+
+    Population* printStats(std::string path) {
+        this->calcStats();
+
+        std::ofstream resultsFile;
+        resultsFile.open(path, std::ios_base::app);
+
+        if (resultsFile.is_open()) {
+            resultsFile << this->getMaxFitness() << "," << this->getMeanFitness() << "\n";
+            resultsFile.close();
+        } else {
+            std::cout << "Could not write to the results file \n";
+        }
+        std::cout << "The best individual in the population has fitness " << this->getMaxFitness() << "\n";
+        std::cout << "The mean fitness for the population is " << this->getMeanFitness() << "\n";
+        return this;
+    }
+
+    int getMaxFitness() {
+        return this->maxFitness;
+    }
+
+    int getMeanFitness() {
+        return this->meanFitness;
+    }
+
+    void addIndividual(Individual<fitnessType>* individual) {
+        this->individuals.push_back(individual);
+    }
+
+    Population* calcStats() {
+        int total = 0;
+        int fitness;
+        this->maxFitness = 0;
+        for (auto &individual : this->individuals) {
+            fitness = individual->getFitness();
+            if (fitness > this->maxFitness) {
+                this->maxFitness = fitness;
+            }
+            total += fitness;
+        }
+        this->meanFitness = static_cast<fitnessType>(total / this->individuals.size());
+        return this;
+    }
+
+    Individual<fitnessType>* getFitestIndividual() {
+        auto fittest = this->individuals.at(0);
+        int fitness;
+        for (auto individual : this->individuals) {
+            fitness = individual->getFitness();
+            if (fitness > fittest->getFitness()) {
+                fittest = individual;
+            }
+        }
+        return fittest;
+    }
+
+    Population* replaceWeakestIndividual(Individual<fitnessType>* fittest) {
+        unsigned long weakest = 0;
+        int fitness;
+        for (unsigned long i = 0; i < this->individuals.size(); i++) {
+            fitness = this->individuals.at(i)->getFitness();
+            if (fitness < this->individuals.at(weakest)->getFitness()) {
+                weakest = i;
+            }
+        }
+        this->individuals.insert(this->individuals.begin() + weakest, fittest);
+    }
+
+    Individual<fitnessType>* cloneFittestIndividual() {
+        auto fittestIndividual = this->getFitestIndividual();
+        int size = fittestIndividual->getSize();
+        auto fittestChromosomes = new int[size];
+        memcpy(fittestChromosomes, fittestIndividual->getChromosomes(), size * sizeof(int));
+        fittestIndividual = new Individual<fitnessType> (size, fittestIndividual->getFitnessStrategy());
+        fittestIndividual->setChromosomes(fittestChromosomes);
+        return fittestIndividual;
+    }
 
 private:
+
     int maxFitness;
     int meanFitness;
 };
