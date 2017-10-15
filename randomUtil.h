d9232cca2fdf542e17afbbb443489022325bd125
Bryan Kneis <b.kneis2011@gmail.com>
2017-10-15 16:48:53 +0100

    Add doc block

--- randomUtil.h
+++ randomUtil.h
@@ -3,6 +3,13 @@
 
 #include <cstdlib>
 
+/**
+ * Generate a random number of a given type between min and max
+ * @tparam T
+ * @param min
+ * @param max
+ * @return
+ */
 template <typename T = int>
 inline T getRandomNumber(int min, int max) {
     return static_cast<T>(rand() % (max + 1) + min);
