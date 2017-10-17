d68fab1d1bd423ef668dd20aed213acce4731f4f
Bryan Kneis <b.kneis2011@gmail.com>
2017-10-17 15:01:20 +0100

    Create rules engine class to create rule base and match data to ruleset

---
+++ RulesEngine.h
@@ -0,0 +1,14 @@
+//
+// Created by arthur on 17/10/17.
+//
+
+#ifndef GENETICALGORITHM_RULESENGINE_H
+#define GENETICALGORITHM_RULESENGINE_H
+
+
+class RulesEngine {
+
+};
+
+
+#endif //GENETICALGORITHM_RULESENGINE_H
