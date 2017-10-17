a2f4ad6190e6a7a428670ec7e9429aad3b589e18
Bryan Kneis <b.kneis2011@gmail.com>
2017-10-17 12:08:45 +0100

    Add the ingester

---
+++ Ingester.cpp
@@ -0,0 +1,5 @@
+//
+// Created by arthur on 17/10/17.
+//
+
+#include "Ingester.h"
---
+++ Ingester.h
@@ -0,0 +1,14 @@
+//
+// Created by arthur on 17/10/17.
+//
+
+#ifndef GENETICALGORITHM_INGESTER_H
+#define GENETICALGORITHM_INGESTER_H
+
+
+class Ingester {
+
+};
+
+
+#endif //GENETICALGORITHM_INGESTER_H
