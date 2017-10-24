a75928f66880af4352297c9edb652477940f3c91
Bryan Kneis <b.kneis2011@gmail.com>
2017-10-24 15:22:45 +0100

    Use data config

--- RulesEngine.cpp
+++ RulesEngine.cpp
@@ -21,7 +21,7 @@ rule *RulesEngine::generateRuleBase(int* chromosome) {
 int RulesEngine::checkRules(data *dataIn, rule* rulesBase) {
     int numMatches = 0;
 
-    for (int i = 0; i < 32; i++) {
+    for (int i = 0; i < NUMBER_OF_DATA; i++) {
         for (int j = 0; j < NUM_RULES; j++) {
             if (isEqual(dataIn[i].var, rulesBase[j].condition, dataIn[0].size)) {
                 if (dataIn[i].group == rulesBase[i].output) {
