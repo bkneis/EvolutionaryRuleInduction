bd3c1b15248355ade52580f38973e5df13deeead
Bryan Kneis <b.kneis2011@gmail.com>
2017-10-24 15:24:31 +0100

    Update ingester to use configured data values

--- Ingester.cpp
+++ Ingester.cpp
@@ -1,21 +1,22 @@
 #include <fstream>
 #include <iostream>
 #include "Ingester.h"
+#include "config.h"
 
 data* Ingester::readFile(const char* path) {
     std::ifstream file(path);
     std::string str;
-    auto dataIn = new data[32];
+    auto dataIn = new data[NUMBER_OF_DATA];
     unsigned j = 0;
     while (std::getline(file, str))
     {
-        data dataItem = { new int[5], 5, 0 };
+        data dataItem = { new int[DATA_LENGTH], DATA_LENGTH, 0 };
         int bit;
-        for (unsigned long i = 0; i < 5; i++) {
-            bit = (int) str.at(i) - 48;
+        for (unsigned long i = 0; i < DATA_LENGTH; i++) {
+            bit = (int) str.at(i) - 48; // Convert ascii string to int
             dataItem.var[i] = bit;
         }
-        dataItem.group = (int) str.at(6) - 48;
+        dataItem.group = (int) str.at(DATA_LENGTH + 1) - 48; // Get the class value
         dataIn[j] = dataItem;
         j++;
     }
